2befb6a34f434ac4c2e2873faf3c4dde971ede5e
Oleg <brezitskyyoleg@gmail.com>
2021-06-06 16:06:12 +0200

    Added ValueError for bad parameter

--- src/main/fibonacci_rabbits.py
+++ src/main/fibonacci_rabbits.py
@@ -8,6 +8,8 @@ def fibonacci_rabbits(n: int) -> int:
     Returns:
         int: number of rabbit pairs after n months.
     """
+    if n < 0:
+        raise ValueError("Months number should be equal or greater than 0.")
     parent = 1
     child = 1
     for _ in range(n):
